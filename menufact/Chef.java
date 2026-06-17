5e58673e794f4d93a28b5078986d2c9c9ac1fb84
YohanDallaire <158216938+YohanDallaire@users.noreply.github.com>
2026-06-17 01:00:10 -0400

    JavaDoc - Chef - FactureObserver - Inventaire - InventaireService

--- menufact/Chef.java
+++ menufact/Chef.java
@@ -6,15 +6,35 @@ import menufact.plats.Etat.EnPreparation;
 import menufact.plats.PlatChoisi;
 import org.jspecify.annotations.NonNull;
 
+/**
+ * Classe pour un chef de cuisine
+ *
+ * <p>
+ *     Le chef de cuisine reçoit des notifications pour les nouveaux plats à préparer
+ * </p>
+ */
 public class Chef implements FactureObserver {
+
+    /** Nom du chef */
     private String nom;
 
+    /**
+     * Constructeur de chef.
+     * @param nom nom du chef.
+     */
     public Chef(String nom) {
         this.nom = nom;
     }
 
 
-
+    /**
+     * Met à jour la commande
+     *
+     * <p>
+     *     Ajout de plats à la facture, modifie l'état des plats.
+     * </p>
+     * @param plat plat à préparer
+     */
     @Override
     public void mettreAJour(@NonNull PlatChoisi plat) {
         System.out.println("[CUISINE - " + nom + "] Préparation demandée pour : "
@@ -23,6 +43,10 @@ public class Chef implements FactureObserver {
         plat.setEtat(new EnPreparation());
     }
 
+    /**
+     * Obtenir le nom du chef
+     * @return le nom du chef
+     */
     public String getNom() {
         return nom;
     }
--- menufact/facture/FactureObserver.java
+++ menufact/facture/FactureObserver.java
@@ -2,6 +2,14 @@ package menufact.facture;
 
 import menufact.plats.PlatChoisi;
 
+/**
+ * Observateur de la facture pour notifier les abonnés d'un changement
+ */
 public interface FactureObserver {
+
+    /**
+     * Envoyer la notification aux abonnés (Chef)
+     * @param plat le plat qui a été ajouté ou modifier
+     */
     void mettreAJour(PlatChoisi plat);
 }
